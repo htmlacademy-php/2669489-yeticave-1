1cd5d8fd6e011bb6221130b9e662f9c05de503e6
sk-web-repository <93796175+sk-web-repository@users.noreply.github.com>
2025-10-02 05:34:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,19 @@ $lots = [
     ],
 ];
 
+function formatThePrice ($num) // объявление функции форматирования цены
+{
+    $rounded_num = ceil($num); // округление цены до целого числа
+
+    if ($rounded_num < 1000) {  // условие для цены < 1000
+        return "{$rounded_num}  ₽";    // возврат цены при условии < 1000, добавление знака валюты
+    }
+
+    return number_format($rounded_num, 0, '', ' ') . ' ₽'; // возврат цены при >= 1000, параметры number, decimals, decimalpoint, separator
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -135,9 +147,11 @@ $lots = [
             <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    
-                    <img src="<?= $lot['image_url'] ?>" width="350" height="260" alt="<?php echo $lot['image_url'] ?>">
 
+                    <!-- вывод имени лота -->
+                
+                    <img src="<?= $lot['image_url'] ?>" width="350" height="260" alt="<?php echo $lot['name'] ?>">
+            
                 </div>
                 <div class="lot__info">
 
@@ -147,7 +161,10 @@ $lots = [
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?php echo $lot['price'] ?><b class="rub">р</b></span>
+
+                            <!-- вызов функции форматирования цены лота, вывод цены лота -->
+
+                            <span class="lot__cost"><?php echo formatThePrice($lot['price'])?></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -227,3 +244,4 @@ $lots = [
 <script src="script.js"></script>
 </body>
 </html>
+
